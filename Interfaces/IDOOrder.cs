fe02721bf5997a5717e2c83df7f0c270f24e8fae
jesdegar <jesdegar@gmail.com>
2021-01-28 21:54:25 +0700

    update IDOOrder

--- Interfaces/IDOOrder.cs
+++ Interfaces/IDOOrder.cs
@@ -4,25 +4,11 @@ using System;
 namespace GoLogs.Interfaces
 {
     // ReSharper disable once InconsistentNaming
-    public interface IDOOrder : IEntity
+    public interface IDOOrder :  IEntity
     {
         /// <summary>
         ///     Universally unique identifier of this <see cref="IDOOrder"/>.
-        /// </summary>
-        Guid? Uuid { get; }
         
-        /// <summary>
-        ///     The BL number for this delivery order.
-        /// </summary>
-        string BlNumber { get; }
-        
-        /// <summary>
-        ///     The BL date for this delivery order.
-        /// </summary>
-        DateTime BlDate { get; }
-        /// <summary>
-        /// The DO Order Number 
-        /// </summary>
         string DOOrderNumber { get; }
         /// <summary>
         /// Cargo Owner Id who request DO Order
